c4dfa87883e40c50738ab6804708758b166664ef
VaidasMotiejunas <vaidomotiejuno@gmail.com>
2017-12-20 11:57:28 +0200

    comitinu paruostus nd failus

---
+++ phpnd5index.php
@@ -0,0 +1,23 @@
+<<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Namu darbas nr 5</title>
+</head>
+<body>
+<?php
+
+
+
+
+
+
+
+
+
+
+?>
+</body>
+</html>
