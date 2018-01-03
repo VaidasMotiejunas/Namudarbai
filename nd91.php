615502986c6d6d3a9e60a26463cba932333c9fd1
VaidasMotiejunas <vaidomotiejuno@gmail.com>
2018-01-03 17:29:29 +0200

    Atliktas 9nd

---
+++ nd91.php
@@ -0,0 +1,30 @@
+<?php
+
+class Radar
+{
+    public $dateOfEvent; //data ir laikas
+    public $plateNumber; //automobilio numeris
+    public $distance; //nuvaziuotas atstumas metrais
+    public $time; //sugaistas laikas
+
+    public function __construct(string $plateNumber, string $dateOfEvent, float $distance,float $time){
+        $this->dateOfEvent = $dateOfEvent;
+        $this->plateNumber = $plateNumber;
+        $this->distance = $distance;
+        $this->time = $time;
+    }
+
+    public function getSpeed() {
+        $distance = $this->distance;
+        $time = $this->time;
+        return round($speed = $distance / $time * 3.6, 1);
+    }
+}
+
+function sortEvents (array $array){
+    usort ($array, function ($a, $b) {
+        return ($a->getSpeed() < $b->getSpeed()); 
+    });
+}
+
+?>
--- nd91index.php
+++ nd91index.php
@@ -55,10 +55,6 @@
         background-color: #f2f2f2;
         padding: 20px;
     }
-    body {
-        background: url("https://ifoto.delfi.lt/show_display.php?id=7328136&width=1200&height=1200&mode=-1") left top;
-        background-size: 225px 150px;
-    }
     table {
         width: 100%;
         padding: 12px 20px;
@@ -79,13 +75,11 @@
     <title>Namu darbas nr 9</title>
 </head>
 <body>
-<?php 
-require 'nd9.php';
-?>
+<?php require 'nd91.php'; ?>
 <h1>Automobiliu registracija</h1>
 
 <div>
-    <form action ="nd9.php" method ="post">
+    <form action ="nd91index.php" method ="post">
         <label for = "data">Ivykio data</label>
         <input id = "data" name ="dateOfEvent" type = "date" placeholder = "Iveskite ivykio data"> <br>
         <label for = "numeris">Valstybinis automobilio numeris</label>
@@ -99,6 +93,33 @@ require 'nd9.php';
 </div>
 <br>
 
+<?php 
+
+if (!isset($_COOKIE)) {
+    $cookieId = 0;
+} else {
+    $cookieId = count($_COOKIE) / 4;
+}
+
+if (!($_REQUEST['dateOfEvent']) || !($_REQUEST['plateNumber']) || !($_REQUEST['distance']) || !($_REQUEST['time'])) {
+    echo "Uzpildykite visus laukelius";
+    //header("Location: {$_SERVER['HTTP_REFERER']}"); //Grazina i pries tai buvusi psl.
+} else {
+    setcookie($cookieId.'plateNumber', $_REQUEST['plateNumber']);
+    setcookie($cookieId.'dateOfEvent', $_REQUEST['dateOfEvent']);
+    setcookie($cookieId.'distance', $_REQUEST['distance']);
+    setcookie($cookieId.'time', $_REQUEST['time']);
+}
+
+$eventObj = [];
+for ($i=0; $i < count($_COOKIE)/4; $i++) {
+    array_push ($eventObj, new Radar($_COOKIE[$i.'plateNumber'], $_COOKIE[$i.'dateOfEvent'], $_COOKIE[$i.'distance'], $_COOKIE[$i.'time']));
+}
+
+usort ($eventObj, function ($a, $b) { //Jei naudoju sortEvents nerusiuoja array.
+    return ($a->getSpeed() < $b->getSpeed()); 
+});
+?>
 <table>
 <thead>
 <tr>
@@ -108,10 +129,17 @@ require 'nd9.php';
 </tr>
 </thead>
 <tbody>
-<?php ?>
 </tbody>
+<?php
+foreach ($eventObj as $radar) {
+    $speed = round($radar->distance / $radar->time * 3.6, 1);
+    echo "<tr>
+            <td>$radar->dateOfEvent</td>
+            <td>$radar->plateNumber</td>
+            <td>$speed</td>
+        </tr>";
+} //treciam <td> neleidzia naudoti $radar->getSpeed();
+?>
 </table>
-
-
 </body>
 </html>
