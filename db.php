7061c5ec96f419e6a25a8ffc43052e2ebec04e07
VaidasMotiejunas <vaidomotiejuno@gmail.com>
2018-01-09 11:01:51 +0200

    Pradedu nd

---
+++ db.php
@@ -0,0 +1,20 @@
+<?php
+
+function connectDB() {
+    $servername = 'localhost';
+    $dbname = 'nd11';
+    $username = 'Auto';
+    $password = 'LabaiSlaptas123';
+
+
+    // Create connection 
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Check connection 
+    if ($conn->connect_error) {
+        die('Nepavyko prisijungti: ' .$conn->connect_error);
+    } echo "Pavyko prisijungti";
+    return $conn;
+}
+
+?>
